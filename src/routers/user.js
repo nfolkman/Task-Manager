c6b6500ff53f3a3d7c18f13f31197f7eb831fd78
nfolkman <nathanielfolkman@gmail.com>
2022-06-20 18:54:48 -0500

    second stretch

--- src/routers/user.js
+++ src/routers/user.js
@@ -36,5 +36,236 @@ router.post('/user', async (req, res) => {
 
 
 
-// Sign-In & Out User Routines
 
+//// Sign-In & Out User Routines////
+
+
+// sign-in
+router.post('/users/login', async (req, res) => {
+   
+   try {
+      const user = await User.findByCredentials(
+         req.body.email,
+
+         req.body.password
+      )
+      const token = await user.generateAuthToken()
+
+      res.send( {
+         user,
+
+         token,
+      })
+
+   } catch ( e ) {
+
+      res.status(400).send({
+         error: 'Catch error',
+         e,
+      });
+
+   }
+   
+})  
+
+
+// sign-out
+router.post( 'users/logout', auth, async (req, res) => {
+
+   try {
+
+      req.user.tokens = req.user.tokens.filter((token)=> {
+         return token.token !== req.token
+      });
+
+      await req.user.save()
+
+      res.status(200).send()
+
+   } catch (e) {
+
+      res.status(500).send()
+
+   }
+})
+
+
+// sign-out all
+router.post( '/users/logoutAll', auth, async (req, res) => {
+
+   try {
+
+      req.user.tokens = []
+
+      await req.user.save()
+
+      res.status(200).send()
+
+   } catch(e) {
+
+      res.status(500).send()
+
+   }
+
+})
+
+
+
+
+
+////RUD (Read, Update, Delete) Operations for User Profile////
+
+
+// Read operation
+router.get( '/users/me', auth, async (req, res) => {
+
+   res.send(req.user)
+
+})
+
+
+// Update validation & operation
+router.patch('/users/me', auth, async (req,res) => {
+
+   const updates = Object.keys(req.body)
+
+   const allowUpdates = ['name', 'email', 'password', 'age']
+
+   const isValidOperation = updates.every((update) => {
+
+      allowedUpdates.includes(update)
+      
+   })
+
+   if(!isValidOperation) {
+
+      return res.status(401).send( {error: 'Updates invalid.'})
+
+   }
+
+   try{
+
+      updates.forEach((update) => ( req.user[update] = req.body[update] ))
+      
+      await req.user.save()
+
+      res.status(201).send(req.user)
+
+   } catch(e) {
+
+      res.status(404).send( {
+         e,
+      })
+
+   }
+
+})
+
+
+// Delate operation
+router.delete( '/users/me', auth, async (req,res) => {
+
+   try {
+
+      await req.user.remove()
+
+      sendCancelationEmail(req.user.email, req.user.name)
+
+      res.send()
+
+   } catch(e) {
+
+      res.status(500).send({e: 'Catch Error', e})
+
+   }
+
+})
+
+
+
+
+////Add, Delete, Fetch User Avatar (Profile Picture) (using Multer package for uploads)////
+
+//process & upload avatar
+const upload = multer( {
+
+   limits: {
+
+      fileSize: 1000000,
+
+   },
+
+   fileFilter (req,file, cb) {
+
+      if(!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+         return cb(new Error( 'Image upload must be in jpg, jpeg, or png file format.'))
+      }
+
+      cb(undefined, true)
+
+   },
+
+})
+
+// add avatar
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+
+      const buffer = await sharp(req.file.buffer).resize( {width:180, height:180} ).jpeg().toBuffer()
+
+      req.user.avatar = buffer
+
+      await req.user.save()
+
+      res.send()
+
+   },
+
+   (error, req, res, next) => {
+
+      res.status(400).send({
+
+         error: error.message,
+
+      })
+
+   }
+
+)
+
+// delete avatar
+router.delete('/users/me/avatar', auth, async (req, res) => {
+
+   req.user.avatar = undefined
+
+   await req.user.save()
+
+   res.send()
+
+})
+
+// fetch avatar
+res.get('/users/:id/avatar', async (req, res) => {
+
+   try{
+
+      const user = await User.findById(req.params.id)
+
+      if(!user || !user.avatar){
+
+         throw new Error()
+
+      }
+
+      res.set('Content-Type', 'image/jpg')
+
+      res.send(user.avatar)
+
+   } catch(e) {
+
+      res.status(404).send()
+
+   }
+
+})
+
+module.exports = router
